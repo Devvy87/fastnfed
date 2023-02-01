f2a1eac0134cbd28a5308f112faff9bcd6902f96
Vladislav <luckyriver87@gmail.com>
2023-02-01 16:49:23 +0300

    add: adaptive layout for OurMenu component

--- src/pages/Menu/OurMenu.js
+++ src/pages/Menu/OurMenu.js
@@ -4,6 +4,8 @@ import './OurMenu.css'
 const OurMenu = () => {
     
   return (
+    
+
     <div className='menu'>
         <h1 className="menuTitle">Our Menu</h1>
         <div className="menuList">
